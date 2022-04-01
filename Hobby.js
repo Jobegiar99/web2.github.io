a7b40112e14d80b5b43b5f34f9cde27c3a585971
Jobegiar99 <44796423+Jobegiar99@users.noreply.github.com>
2022-04-01 15:45:49 -0600

    Delete Hobby.js

--- Hobby.js
+++
@@ -1,6 +0,0 @@
-const Hobby = Vue.component("hobby",
-    {
-        props: ['h','n'],
-        template: '<div class="hobby" align = "center"> <div class = "row"> <div class = "col-12"><br>{{ n }} likes to {{ h }}</div> </div></div>'
-    }
-);
