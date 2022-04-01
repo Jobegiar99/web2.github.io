8115323de38fae8d6e4975cdb23f1c76d1360d99
Jobegiar99 <44796423+Jobegiar99@users.noreply.github.com>
2022-04-01 15:45:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        name: "Jobegiar",
-        hobbies:["compose music","play the piano","sing",
-                            "develop projects related to game AI","exercise","read"],
-        showHobbies: true,
-        message: "Welcome to my super page",
-        showPlaces: true,
-        secondName: "Not Jobegiar",
-        places:["Irland","Japan","United Kingdom","Colombia"],
-        secondMessage:" HOLAAA "
+const app = Vue.createApp({
+    data() {
+        return{
+            name: "Jobegiar",
+            hobbies:["compose music","play the piano","sing",
+                                "develop projects related to game AI",
+                                "exercise","read"],
+            showHobbies: true,
+            message: "Welcome to my super page",
+            showPlaces: true,
+            secondName: "Not Jobegiar",
+            places:["Irland","Japan","United Kingdom","Colombia"],
+            secondMessage:" HOLAAA "
+        }
     }
-  });
+  }).mount("#app")
