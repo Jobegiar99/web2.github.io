7b6ee3473897d6d0e985a3181c98f9a4a9090888
Jobegiar99 <44796423+Jobegiar99@users.noreply.github.com>
2022-04-01 15:45:39 -0600

    Delete Tour.js

--- Tour.js
+++
@@ -1,6 +0,0 @@
-const Tour = Vue.component("tour",
-    {
-        props: ['p','s'],
-        template: '<div class="tour" align = "center"> <div class = "row"> <div class = "col-12"><br>{{s}} wants to visit {{p}}</div> </div></div>'
-    }
-);
